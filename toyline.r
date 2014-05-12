c5d88b619262568d6d4ce81f3e734b03c39b7e17
Roza Bidshahri <roza.bidshahri@gmail.com>
2014-05-12 14:25:09 -0700

    Morning Session

---
+++ toyline.r
@@ -0,0 +1,11 @@
+a <- 4
+b <- 1.2
+sigSq <- 0.4
+x <- runif(40)
+y<-a+b*x+rnorm(40,sd=sqrt(0.4))
+rnorm(40,sd=sqrt(0.4))
+plot(x,y)
+abline(a=a,b=b,col="orange",lwd=2)
+dev.print(pdf,"toyline-linear.pdf")
+dev
+
